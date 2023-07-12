a0a5a1b3bc675274b5c6b96856c789ed1f06a0df
German Encina <gencina2@gmail.com>
2023-07-11 22:10:47 -0400

    Con validacion de que se ingrese un nùmero con punto

--- app/src/main/java/cl/gencina/conversortemperatura/MainActivity.kt
+++ app/src/main/java/cl/gencina/conversortemperatura/MainActivity.kt
@@ -1,30 +1,28 @@
 package cl.gencina.conversortemperatura
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-
 fun main(){
+    inicial()
+}
+
+fun inicial(){
     println("Ingrese la temperatura en celcius")
     val temperaturaInicial = readLine()
 
-    val celcius = temperaturaInicial?.toDouble()
-    val farenheit = (celcius!! * 9/5) + 32
-    val kelvin = 273.15 + celcius
-    val farenheitToKelvin = (5.0/9.0) * (farenheit - 32)+ 273.15
+    if(isNumber(temperaturaInicial!!)) {
+        val celcius = temperaturaInicial.toDouble()
+        val farenheit = (celcius * 9 / 5) + 32
+        val kelvin = 273.15 + celcius
+        val farenheitToKelvin = (5.0 / 9.0) * (farenheit - 32) + 273.15
 
-   println( "${String.format("%.2f", celcius )} ºC" )
-   println( "${String.format("%.2f", farenheit )} ºF" )
-   println( "${String.format("%.2f", kelvin )} ºK" )
-   println( "${String.format("%.2f", farenheitToKelvin )} ºK" )
+        println("${String.format("%.2f", celcius)} ºC")
+        println("${String.format("%.2f", farenheit)} ºF")
+        println("${String.format("%.2f", kelvin)} ºK")
+        println("${String.format("%.2f", farenheitToKelvin)} ºK")
+    }else{
+        inicial()
+    }
 }
 
 fun isNumber(string: String): Boolean {
-    return string.none { it in '0'..'9' }
+    return string.none { it !in '0'..'9' }
 }
-
-/*class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
-}*/
